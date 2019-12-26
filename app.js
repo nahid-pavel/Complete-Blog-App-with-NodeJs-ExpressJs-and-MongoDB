146fb0b172b52c64c2568a8a41dad88c34b30084
nahid-pavel <pavelavw@yahoo.com>
2019-12-26 19:23:03 +0600

    changes in db config

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((error,req,res,next)=>{
 PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(MONGODB_URI, {
+  .connect(process.env.MONGODB_URI || MONGODB_URI, {
     useNewUrlParser: true
   })
   .then(() => {
